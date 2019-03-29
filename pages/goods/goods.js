a2f9544456f871ee4a098d066bf06f4c7bdc3d05
Thruman <1006556989@qq.com>
2019-03-29 17:39:22 +0800

    海报初步

--- pages/goods/goods.js
+++ pages/goods/goods.js
@@ -5,10 +5,243 @@ var util = require('../../utils/util.js');
 var api = require('../../config/api.js');
 var user = require('../../utils/user.js');
 
+
+const posterConfig = {
+  jdConfig: {
+    width: 750,
+    height: 1000,
+    backgroundColor: '#fff',
+    debug: false,
+    blocks: [{
+        width: 690,
+        height: 808,
+        x: 30,
+        y: 183,
+        borderWidth: 2,
+        borderColor: '#f0c2a0',
+        borderRadius: 20,
+      },
+      {
+        width: 634,
+        height: 74,
+        x: 59,
+        y: 770,
+        backgroundColor: '#fff',
+        opacity: 0.5,
+        zIndex: 100,
+      },
+    ],
+    texts: [{
+        x: 113,
+        y: 61,
+        baseLine: 'middle',
+        text: '聂祥1',
+        fontSize: 32,
+        color: '#8d8d8d',
+      },
+      {
+        x: 30,
+        y: 113,
+        baseLine: 'top',
+        text: '',
+        fontSize: 38,
+        color: '#080808',
+      },
+      {
+        x: 92,
+        y: 810,
+        fontSize: 38,
+        baseLine: 'middle',
+        text: '聂祥3',
+        width: 570,
+        lineNum: 1,
+        color: '#8d8d8d',
+        zIndex: 200,
+      },
+      {
+        x: 59,
+        y: 895,
+        baseLine: 'middle',
+        text: [{
+            text: '2人拼',
+            fontSize: 28,
+            color: '#ec1731',
+          },
+          {
+            text: '¥99',
+            fontSize: 36,
+            color: '#ec1731',
+            marginLeft: 30,
+          }
+        ]
+      },
+      {
+        x: 522,
+        y: 895,
+        baseLine: 'middle',
+        text: '已拼2件',
+        fontSize: 28,
+        color: '#929292',
+      },
+      {
+        x: 59,
+        y: 945,
+        baseLine: 'middle',
+        text: [{
+            text: '商家发货&售后',
+            fontSize: 28,
+            color: '#929292',
+          },
+          {
+            text: '七天退货',
+            fontSize: 28,
+            color: '#929292',
+            marginLeft: 50,
+          },
+          {
+            text: '运费险',
+            fontSize: 28,
+            color: '#929292',
+            marginLeft: 50,
+          },
+        ]
+      },
+      {
+        x: 360,
+        y: 1065,
+        baseLine: 'top',
+        text: '长按识别小程序码',
+        fontSize: 38,
+        color: '#080808',
+      },
+      {
+        x: 360,
+        y: 1123,
+        baseLine: 'top',
+        text: '超值好货一起拼',
+        fontSize: 28,
+        color: '#929292',
+      },
+    ],
+    images: [{
+        width: 62,
+        height: 62,
+        x: 30,
+        y: 30,
+        borderRadius: 62,
+        url: 'https://lc-I0j7ktVK.cn-n1.lcfile.com/02bb99132352b5b5dcea.jpg',
+      },
+      {
+        width: 634,
+        height: 634,
+        x: 59,
+        y: 210,
+        url: 'https://lc-I0j7ktVK.cn-n1.lcfile.com/193256f45999757701f2.jpeg',
+      },
+      {
+        width: 220,
+        height: 220,
+        x: 92,
+        y: 1020,
+        url: 'https://lc-I0j7ktVK.cn-n1.lcfile.com/d719fdb289c955627735.jpg',
+      }
+    ]
+
+  },
+  demoConfig: {
+    width: 750,
+    height: 1000,
+    backgroundColor: '#fff',
+    debug: false,
+    blocks: [{
+      x: 0,
+      y: 10,
+      width: 750, // 如果内部有文字，由文字宽度和内边距决定
+      height: 120,
+      paddingLeft: 0,
+      paddingRight: 0,
+      borderWidth: 10,
+      borderColor: 'red',
+      backgroundColor: 'blue',
+      borderRadius: 40,
+      text: {
+        text: [{
+            text: '金额¥ 1.00',
+            fontSize: 80,
+            color: 'yellow',
+            opacity: 1,
+            marginLeft: 50,
+            marginRight: 10,
+          },
+          {
+            text: '金额¥ 1.00',
+            fontSize: 20,
+            color: 'yellow',
+            opacity: 1,
+            marginLeft: 10,
+            textDecoration: 'line-through',
+          },
+        ],
+        baseLine: 'middle',
+      },
+    }],
+    texts: [{
+        x: 0,
+        y: 180,
+        text: [{
+          text: '长标题长标题长标题长标题长标题长标题长标题长标题长标题',
+          fontSize: 40,
+          color: 'red',
+          opacity: 1,
+          marginLeft: 0,
+          marginRight: 10,
+          width: 200,
+          lineHeight: 40,
+          lineNum: 2,
+        }],
+        baseLine: 'middle',
+      }
+
+    ],
+    images: [{
+        url: 'https://lc-I0j7ktVK.cn-n1.lcfile.com/02bb99132352b5b5dcea.jpg',
+        width: 300,
+        height: 300,
+        y: 450,
+        x: 0,
+        // borderRadius: 150,
+        // borderWidth: 10,
+        // borderColor: 'red',
+      },
+      {
+        url: 'https://lc-I0j7ktVK.cn-n1.lcfile.com/02bb99132352b5b5dcea.jpg',
+        width: 100,
+        height: 100,
+        y: 450,
+        x: 400,
+        borderRadius: 100,
+        borderWidth: 10,
+      },
+    ],
+    lines: [{
+      startY: 800,
+      startX: 10,
+      endX: 300,
+      endY: 800,
+      width: 5,
+      color: 'red',
+    }]
+
+  }
+}
+
+
+
 Page({
   data: {
     id: 0,
     goods: {},
+    posterConfig: posterConfig.jdConfig,
     attributeList: [],
     issueList: [],
     comment: [],
@@ -27,10 +260,10 @@ Page({
     collectImage: '/static/images/icon_collect.png',
     shareImage: '',
     soldout: false,
-    collectStatus:"收藏"
+    collectStatus: "收藏"
   },
 
-  onReady: function () {
+  onReady: function() {
     // 页面渲染完成
     this.sharePop = this.selectComponent("#sharePop");
     this.notify = this.selectComponent("#van-notify");
@@ -42,16 +275,16 @@ Page({
     wx.hideNavigationBarLoading() //完成停止加载
     wx.stopPullDownRefresh() //停止下拉刷新
   },
-  
-  onShow: function () {
+
+  onShow: function() {
     // 页面显示
-    if (app.globalData.hasLogin){
+    if (app.globalData.hasLogin) {
       this.getCollectStatus();
     }
   },
 
 
-  onLoad: function (options) {
+  onLoad: function(options) {
     // 页面初始化 options为页面跳转所带来的参数
     if (options.id) {
       this.setData({
@@ -142,6 +375,20 @@ Page({
         wx.hideLoading();
       }
     });
+  },
+
+  onPosterSuccess(e) {
+    console.log(e);
+    const {
+      detail
+    } = e;
+    wx.previewImage({
+      current: detail,
+      urls: [detail]
+    })
+  },
+  onPosterFail(err) {
+    console.error(err);
   },
 
   // 获取推荐商品 没有问题
@@ -341,13 +588,13 @@ Page({
       });
   },
   //收藏状态
-  getCollectStatus: function () {
+  getCollectStatus: function() {
     let that = this;
     util.request(api.CollectStatus, {
-      type: 0,
-      valueId: this.data.id
-    }, "POST")
-      .then(function (res) {
+        type: 0,
+        valueId: this.data.id
+      }, "POST")
+      .then(function(res) {
         let _res = res;
         if (_res.errno == 0) {
           if (_res.data.status == 0) {
@@ -368,7 +615,7 @@ Page({
         }
       });
   },
-  
+
   cutNumber: function() {
     this.setData({
       number: (this.data.number - 1 > 1) ? this.data.number - 1 : 1
